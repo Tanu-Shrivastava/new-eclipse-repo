f583e09730001bec9122161277f1c43d5815a94c
Tanu Shrivastava <tanushrivastava0106@gmail.com>
2020-12-18 10:13:44 +0530

    2nd commit

--- LTI_corejava/src/demo1/Add.java
+++ LTI_corejava/src/demo1/Add.java
@@ -3,8 +3,8 @@ package demo1;
 public class Add {
 public static void main(String args[]){
 
-	int a=10;
-	int b=20;
+	int a=66;
+	int b=200;
 	int c=a+b;
 System.out.println("sum is " + c);
 System.out.println("Addition of "+ a+"and " + b +" is " + c);
